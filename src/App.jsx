f451fcde81d3a245d916c220c9e8c8dc2576136f
Varshil2507 <varshil.patel@orionik.com>
2025-05-21 17:22:07 +0530

    ThemeToggle and animation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "./pages/home"
+import { NotFound } from "./pages/NotFound"
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React + Tailwind CSS</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path="*" element = { <NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+export const NotFound = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <p>Page not found</p>
+            <p>Sorry, the page you are looking for does not exist.</p>
+            <a href="/">Go back to home</a>
+        </div>
+    )
+}
