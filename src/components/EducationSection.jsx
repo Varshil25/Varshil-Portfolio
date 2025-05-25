4502665065ac5cd32d77ec34ea5a2bfb424146d3
Varshil25 <187360177+Varshil2507@users.noreply.github.com>
2025-05-25 18:49:29 +0530

    init setup of Education

---
+++ src/components/EducationSection.jsx
@@ -0,0 +1,14 @@
+export const EducationSection = () => {
+    return <section>
+        <div>
+            {" "}
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4x1 font-bold mb-1 text-center">
+                     <span className="text-primary"> Education </span>
+                </h2>
+                <hr className="border-t-2 border-[#E5E7EB] w-44 mx-auto mb-12" />
+
+            </div>
+        </div>
+    </section>
+}
